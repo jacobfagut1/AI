0e249e5d13146a2ded4762f118136c588f24a8d3
mjefagut01@tip.edu.ph <mjefagut01@tip.edu.ph>
2025-04-24 14:05:01 +0800

    settings

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -1,23 +1,26 @@
 from pathlib import Path
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_dev_secret')
+SECRET_KEY = 'django-insecure-$sn*ilosstwg2ad=ew#^+lnjpu&@et4#h6k=bblc$h^vws@%r2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1 ai-5k1u.onrender.com').split()
+import os
 
-OPENAI_KEY = os.getenv("OPENAI_KEY")
-MONGO_URI = os.getenv("MONGO_URI")
+SECRET_KEY = os.getenv('!p#8va9@bc5@0+s57w5b)ng0rm!_8l9d(e@x4ie_&7#rc1u**q', 'fallback_dev_secret')
+DEBUG = False
+ALLOWED_HOSTS = ['ai-5k1u.onrender.com', 'localhost', '127.0.0.1']
+
+OPENAI_KEY = os.getenv("859836f69a8d17c8564a55e4e2b112e5")
+MONGO_URI = os.getenv("932628464a9926edfdd4ea611e005463")
 
 # Application definition
 INSTALLED_APPS = [
@@ -59,14 +62,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
-# Database (MongoDB with Djongo)
+# Database
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'ecommerce_db',
         'ENFORCE_SCHEMA': False,
         'CLIENT': {
-            'host': os.getenv('DB_URI')
+            'host': 'mongodb+srv://mjefagut01:secretpassword@cluster0.9lahgmr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
         },
     }
 }
@@ -91,6 +94,16 @@ STATIC_URL = 'static/'
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Debugging helper (optional, for testing)
+import os
 print("BASE DIR:", os.getcwd())
 print("FILES:", os.listdir())
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+
+
+
+
